120ea8e1b7895105993ba6f2f3a18ccb59a584b7
Meena Sundaram <meena@Meenas-Laptop.local>
2023-04-25 18:47:40 -0500

    removing

--- src/main/java/com/summercampquest/campquest/models/data/CampRepository.java
+++
@@ -1,9 +0,0 @@
-package com.summercampquest.campquest.models.data;
-
-import com.summercampquest.campquest.models.Camp;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CampRepository extends CrudRepository<Camp, Integer> {
-}

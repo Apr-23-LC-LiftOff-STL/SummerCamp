21b2289542e1395d3234dce7da29339b29c4d08e
Sydney Jones <sydneyjones87@gmail.com>
2023-04-26 18:28:52 -0500

    troubleshooting front-backend connection errors

--- angular-frontend-camp/src/app/app-routing.module.ts
+++ angular-frontend-camp/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CampListComponent } from './camp-list/tutorials-list.component';
+import { CampListComponent } from './camp-list/camp-list.component';
 
 const routes: Routes = [
 { path: 'camps', component: CampListComponent }
--- angular-frontend-camp/src/app/camp.service.ts
+++ angular-frontend-camp/src/app/camp.service.ts
@@ -8,33 +8,33 @@ import { Camp } from './camp';
 })
 export class CampService {
 
-  private baseURL = "http://localhost:8080/api/camps";
+  private baseUrl = "http://localhost:8080/api/camps";
 
   constructor(private httpClient: HttpClient) { }
-/*
-  getCampsList(): Observable<Camp[]>{
-    return this.httpClient.get<Camp[]>(`${this.baseURL}`);
-  }
-  */
 
-  getAll(): Observable<Camp[]> {
-      return this.http.get<Camp[]>(baseUrl);
+  getCampsList(): Observable<Camp[]>{
+      return this.httpClient.get<Camp[]>(this.baseUrl);
     }
 
-    get(id: any): Observable<Camp> {
-      return this.http.get(`${baseUrl}/${id}`);
+
+  getAll(): Observable<Camp[]> {
+      return this.httpClient.get<Camp[]>(this.baseUrl);
+    }
+
+    get(id: any): Observable<any> {
+      return this.httpClient.get(`${this.baseUrl}/${id}`);
     }
 
     create(data: any): Observable<any> {
-      return this.http.post(baseUrl, data);
+      return this.httpClient.post(this.baseUrl, data);
     }
 
     update(id: any, data: any): Observable<any> {
-      return this.http.put(`${baseUrl}/${id}`, data);
+      return this.httpClient.put(`${this.baseUrl}/${id}`, data);
     }
 
     delete(id: any): Observable<any> {
-      return this.http.delete(`${baseUrl}/${id}`);
+      return this.httpClient.delete(`${this.baseUrl}/${id}`);
     }
 
 

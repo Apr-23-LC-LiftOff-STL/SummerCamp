76e5f4870539a19917bb8aad12dd3ed62d1e0292
Sydney Jones <sydneyjones87@gmail.com>
2023-04-27 18:58:29 -0500

    removed abstractentity after confirming with team

--- spring-backend/src/main/java/com/summercampquest/campquest/models/Camp.java
+++ spring-backend/src/main/java/com/summercampquest/campquest/models/Camp.java
@@ -1,11 +1,11 @@
 package com.summercampquest.campquest.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
-public class Camp extends AbstractEntity{
+public class Camp {
 
     /* @NotBlank(message = "Required")
     @Size(min = 3, max = 50, message = "Must be between 3 and 50 characters")
@@ -19,6 +19,10 @@ public class Camp extends AbstractEntity{
     mode: string;
     category: string;
     campLink: string;*/
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
     @Column(name="name")
     private String name;
 
@@ -48,7 +52,7 @@ public class Camp extends AbstractEntity{
 
     public Camp(String name, String description, Integer price, String duration, Integer age, Date deadline,
                 Integer totalSeats, String mode, String category, String campLink) {
-        super();
+
         this.name=name;
         this.description = description;
         this.price = price;
@@ -61,16 +65,18 @@ public class Camp extends AbstractEntity{
         this.campLink = campLink;
     }
 
-
-    public String getName() {
-        return name;
+    public int getId() {
+        return id;
     }
+
 
     public void setName(String name) {
         this.name = name;
     }
 
-
+    public String getName() {
+        return name;
+    }
 
     public String getDescription() {
         return description;
@@ -144,18 +150,16 @@ public class Camp extends AbstractEntity{
         this.campLink = campLink;
     }
 
-//    @Override
-//    public String toString() {
-//        return "Camp{" +
-//                "description='" + description + '\'' +
-//                ", price=" + price +
-//                ", duration='" + duration + '\'' +
-//                ", age=" + age +
-//                ", deadline=" + deadline +
-//                ", totalSeats=" + totalSeats +
-//                ", mode='" + mode + '\'' +
-//                ", category='" + category + '\'' +
-//                ", campLink='" + campLink + '\'' +
-//                '}';
-//    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Camp camp = (Camp) o;
+        return id == camp.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }

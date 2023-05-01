f92eeaf97237e096bbd6d73d61c5845294af3320
Saranya <sarakrish34@gmail.com>
2023-04-30 22:53:16 -0500

    Fixing errors

--- src/main/java/com/summercampquest/campquest/models/data/CampRepository.java
+++ src/main/java/com/summercampquest/campquest/models/data/CampRepository.java
@@ -1,13 +1,9 @@
-package com.summercampquest.campquest.data;
+package com.summercampquest.campquest.models.data;
 
 import com.summercampquest.campquest.models.Camp;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Id;
-
 @Repository
-
 public interface CampRepository extends CrudRepository<Camp, Integer> {
-
 }

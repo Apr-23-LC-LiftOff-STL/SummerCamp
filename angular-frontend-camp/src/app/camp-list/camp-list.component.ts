ae4a3c8d39eff74735e138872ed3360c434b8c9f
Meena Sundaram <meena@Meenas-Laptop.local>
2023-04-30 20:48:25 -0500

    camp-list.component.ts changed

--- angular-frontend-camp/src/app/camp-list/camp-list.component.ts
+++ angular-frontend-camp/src/app/camp-list/camp-list.component.ts
@@ -8,26 +8,14 @@ import { CampService } from '../camp.service';
   styleUrls: ['./camp-list.component.css']
 })
 export class CampListComponent implements OnInit {
-<<<<<<< HEAD
   
   camps: Camp[] = [];
   constructor(private campService: CampService) { }
-=======
-
-  camps: Camp[] = [];
-
-  constructor(private campService: CampService) { 
-  }
->>>>>>> 35e97f3ab7eaed2768eb1ca27225994cd9786978
 
   ngOnInit(): void {
     this.getCamps();
   }
-<<<<<<< HEAD
   
-=======
-
->>>>>>> 35e97f3ab7eaed2768eb1ca27225994cd9786978
   private getCamps(){
     this.campService.getCampsList().subscribe(data => {
         this.camps = data;

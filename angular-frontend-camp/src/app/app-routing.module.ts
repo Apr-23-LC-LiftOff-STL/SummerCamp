a715c3ac86e8de6ad90de138a147facf25b61a26
Sydney Jones <sydneyjones87@gmail.com>
2023-05-03 14:41:45 -0500

    fixed grammer and label errors

--- angular-frontend-camp/src/app/app-routing.module.ts
+++ angular-frontend-camp/src/app/app-routing.module.ts
@@ -5,19 +5,19 @@ import { CreateCampComponent } from './create-camp/create-camp.component';
 import { LoginComponent } from './login/login.component';
 
 import { FormsModule } from '@angular/forms';
-
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   //javascript object
-  {path:'camps', component: CampListComponent},
-  {path:'create-camp', component: CreateCampComponent},
-  {path:'login',component: LoginComponent},
-  {path:'', redirectTo:'camps', pathMatch:'full'}
+  { path: 'camps', component: CampListComponent },
+  { path: 'create-camp', component: CreateCampComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
+  { path: '', redirectTo: 'camps', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),
-    FormsModule],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), FormsModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
